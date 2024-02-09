2c6d54c5938d2501fd1e893572e087e24d14cdf9
HabTeg21 <81588184+HabTeg21@users.noreply.github.com>
2024-02-08 18:40:52 -0600

    projects update 1

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,8 +8,14 @@ const Projects = () => {
                 <h3>Portfolio</h3>
                 <p>Personal portfolio</p>
                 <p>Technologies used: JavaScript(React), HTML, CSS, Bootstrap </p>
-                <a href="https://github.com/htegegne/portfolio.github.io" target="_blank" rel="noopener noreferrer">PortfolioGitHub Repository</a>
+                <a href="https://github.com/htegegne/portfolio.github.io" target="_blank" rel="noopener noreferrer">Portfolio GitHub Repository</a>
                 {/* Add more projects as needed */}
+                
+                'Gathered and analyzed requirements to meet project expectations.',
+                'Led the development team in setting up the collaboration environment in Bitbucket and ensuring weekly timely delivery, resulting in the successful delivery of a complex project on time. Developed user personas and prototypes to assess and validate user interfaces.',
+                'Created user-friendly interfaces that enhanced user satisfaction and engagement.',
+
+             <a href = " https://bitbucket.org/restaurantws/teamc_hci_repo/src/master/" target="_blank" rel="noopener noreferrer">Bitbucket Repository</a>
             </div>
         </div>
     );
