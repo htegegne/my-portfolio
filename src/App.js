757a535a2b163791a2bfb19a9570b3a886516c1c
HabTeg21 <81588184+HabTeg21@users.noreply.github.com>
2024-02-08 19:31:38 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
