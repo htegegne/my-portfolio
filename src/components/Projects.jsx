14adfc8366a627912d673c05f7b8ad456087cf1f
HabTeg21 <81588184+HabTeg21@users.noreply.github.com>
2024-02-16 07:19:59 -0600

    portfolio project added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,22 +1,31 @@
 import React from 'react';
+import '../styles/projects.css';
 
 const Projects = () => {
+    const projects = [
+        {
+            name:  'Portfolio Website',
+            description:  'Developed a personal portfolio website to showcase my skills, projects, and professional experience.',
+            technologies: ['React', 'HTML', 'CSS', 'Bootstrap'],
+            repoLink: 'https://github.com/htegegne/portfolo',
+            liveDemoLink:  'https://htegegne.github.io/my-portfolio/'
+        }
+    ];
+
     return (
         <div className="section">
             <h2>Projects</h2>
-            <div className="project">
-                <h3>Portfolio</h3>
-                <p>Personal portfolio</p>
-                <p>Technologies used: JavaScript(React), HTML, CSS, Bootstrap </p>
-                <a href="https://github.com/htegegne/portfolio.github.io" target="_blank" rel="noopener noreferrer">Portfolio GitHub Repository</a>
-                {/* Add more projects as needed */}
-                
-                'Gathered and analyzed requirements to meet project expectations.',
-                'Led the development team in setting up the collaboration environment in Bitbucket and ensuring weekly timely delivery, resulting in the successful delivery of a complex project on time. Developed user personas and prototypes to assess and validate user interfaces.',
-                'Created user-friendly interfaces that enhanced user satisfaction and engagement.',
+            <br/>
+            {projects.map((project, index) => (
+                <div key={index}>
+                     <h3>{project.name}</h3>
+                    <p>{project.description}</p>
+                    <p><b>Technologies used: </b>{project.technologies.join(", ")}</p>
+                    <p><b>Repository: </b><a href={project.repoLink} target="_blank" rel="noopener noreferrer">Portfolio GitHub Repository</a></p>
+                    {project.liveDemoLink && <p><b>Live Demo: </b><a href={project.liveDemoLink} target="_blank" rel="noopener noreferrer">{project.liveDemoLink}</a></p>}
+                </div>
 
-             <a href = " https://bitbucket.org/restaurantws/teamc_hci_repo/src/master/" target="_blank" rel="noopener noreferrer">Bitbucket Repository</a>
-            </div>
+            ))}         
         </div>
     );
 };
