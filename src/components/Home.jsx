36212868b910b51b0eb51a6d3763ccb43fe0e4b6
HabTeg21 <81588184+HabTeg21@users.noreply.github.com>
2025-08-12 10:26:59 -0500

    Describe your update here

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,11 +17,15 @@ const Home = () => {
             <div className="content">
                 <h1 className="welcome-text">Welcome to My Portfolio</h1>
                 <h1 className="name">I am Habtamu Tegegne</h1>
-                <h3 className="role">Software Engineer, Full-Stack Developer</h3>
+                <h3 className="role">Software Engineer | Full-Stack Developer | Cloud, DevOps & Database Specialist</h3>
                 <p className="description">
-                   Passionate and results-driven DevOps Engineer and Software Developer with expertise in full-stack development, cloud technologies, and CI/CD automation.
-                    Skilled in designing scalable solutions with Java, C#, JavaScript, AWS, and Kubernetes. Committed to continuous learning and innovation. 
-                    Let’s build something incredible!
+                Highly motivated Software Engineer with a passion for both backend and frontend development, 
+                cloud infrastructure, and DevOps automation. Experienced in building scalable 
+                applications with modern frameworks and managing both relational and NoSQL databases. 
+                Agile team player who’s always eager to learn and improve. 
+                Let’s build something incredible!
+
+
                 </p>
                 <div className="download-section">
                     <button className="button download-btn" onClick={handleDownload}>
